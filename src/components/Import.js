6a229ea280d32fc88881cd57221c27d8c51415cc
Magues17 <mikel.d.campbell@gmail.com>
2023-09-03 21:08:24 -0500

    react redux thunk update

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,10 +1,73 @@
-import React from 'react'
+import  React from 'react'
+import { Container,
+	 Button,
+	  Table, 
+	  TableBody, 
+	  TableCell, 
+	  TableHead, 
+	  TableRow, 
+	  Menu, 
+	  MenuItem } from '@mui/material';
+import { MoreVert } from '@mui/icons-material';
+
 
 const Import = (props) => {
-    // fill out this component
+	
+	const [anchorEl, setAnchorEl] = React.useState(null);
+	const open = Boolean(anchorEl);
+	const handleClick = (event) => {
+	  setAnchorEl(event.currentTarget);
+	};
+	const handleClose = () => {
+	  setAnchorEl(null);
+	};
+	const handleDelete = () => {
+		props.deleteMake();
+		setAnchorEl(null);
+	};
 
     return (
-        <p>Import Component</p>
+        <Container>
+            <Button variant="contained" color="primary" onClick={props.fetchMakes}>
+				Import
+			</Button>
+			<h2>COUNT: {props.makes.length}</h2>
+            <Table>
+			<TableHead>
+				<TableRow>
+					<TableCell>Id</TableCell>
+					<TableCell>Make</TableCell>
+					<TableCell>Actions</TableCell>
+				</TableRow>
+			</TableHead>
+			<TableBody>{props.makes.map((make, index) => (
+				<TableRow key={index}>
+					<TableCell>{make.MakeId}</TableCell>
+					<TableCell>{make.MakeName}</TableCell>
+					<TableCell>
+							<MoreVert
+							id="basic-button"
+							aria-controls={open ? 'basic-menu' : undefined}
+							aria-haspopup="true"
+							aria-expanded={open ? 'true' : undefined}
+							onClick={handleClick}/>
+					</TableCell>
+				</TableRow>
+                        ))}
+		</TableBody>
+			</Table>
+			<Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem onClick={handleDelete}>DELETE</MenuItem>
+      </Menu>
+        </Container>
     )
 }
 
